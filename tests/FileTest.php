06a56c27d375cafd547cff16d43ef4e623981977
LikeAJohny <code@john-at-work.de>
2022-03-04 15:44:21 +0100

    FileTest

---
+++ tests/FileTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpFsTest;
+
+use PhpFs\Directory;
+use PhpFs\Exception\FileException;
+use PhpFs\File;
+use PHPUnit\Framework\TestCase;
+
+class FileTest extends TestCase
+{
+    private const ARTIFACTS = './tests/artifacts';
+    private const FIXTURES = './tests/fixtures';
+
+    public function testCanCreateFile(): void
+    {
+        File::create($file = self::ARTIFACTS . '/cool-story-bro.txt');
+
+        $this->assertFileExists($file);
+    }
+
+    public function testThrowsExceptionOnCreateError(): void
+    {
+        $this->expectException(FileException::class);
+
+        File::create(self::ARTIFACTS . '/dir-does-not-exist/cool-story-bro.txt');
+    }
+
+    public function testCanCopyFileToExistingDirectory(): void
+    {
+        $file = self::FIXTURES . '/content/first-level.txt';
+        $target = self::ARTIFACTS . '/first-level.txt';
+
+        File::copy($file, $target);
+
+        $this->assertFileExists($target);
+    }
+
+    public function testCanMoveFileToExistingDirectory(): void
+    {
+        $file = self::ARTIFACTS . '/mover.txt';
+        $target = self::ARTIFACTS . '/moved.txt';
+
+        File::create($file);
+        File::move($file, $target);
+
+        $this->assertFileExists($target);
+    }
+
+    protected function setUp(): void
+    {
+        Directory::create(self::ARTIFACTS);
+    }
+
+    protected function tearDown(): void
+    {
+        Directory::remove(self::ARTIFACTS);
+    }
+}
